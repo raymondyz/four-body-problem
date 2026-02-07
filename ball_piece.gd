b518a73333d5c6a7647159d4ad24227317bda904
Brendan Aeria <89553639+aeriab@users.noreply.github.com>
2026-02-07 15:54:12 -0800

    debugging line not working all the way

--- ball_piece.gd
+++ ball_piece.gd
@@ -9,13 +9,13 @@ var center_velocity_threshold: float = 3.0
 
 @onready var connect_area_2d: Area2D = $ConnectArea2D
 
-var neighbors: Array[RigidBody2D] = []
+var connected_bodies: Array = []
 @export var line_color: Color = Color.RED
 
 func _draw() -> void:
-	for neighbor in neighbors:
-		if is_instance_valid(neighbor):
-			var local_target = to_local(neighbor.global_position)
+	for body in connected_bodies:
+		if is_instance_valid(body):
+			var local_target = to_local(body.global_position)
 			draw_line(Vector2.ZERO, local_target, line_color, 2.0)
 
 
@@ -39,24 +39,34 @@ func _physics_process(_delta: float) -> void:
 		#low_velocity_timer = 0.0
 
 func _on_body_entered(body):
-	var is_same_color: bool = body.is_in_group("Black")
-	if is_in_group("White"):
-		is_same_color = body.is_in_group("White")
 	
+	if not (body.is_in_group("White") or body.is_in_group("Black")):
+		return
+	var same_group = (is_in_group("White") and body.is_in_group("White")) or (is_in_group("Black") and body.is_in_group("Black"))
 	
-	print("Detected body is same color: ", is_same_color)
+	if same_group and body not in connected_bodies:
+		connected_bodies.append(body)
+		body.connected_bodies.append(self)
+	
 
+func _on_body_exited(body):
+	# Remove from connected bodies if present
+	if body in connected_bodies:
+		connected_bodies.erase(body)
+		body.connected_bodies.erase(self)
 
+@onready var polygon_2d: Polygon2D = $Polygon2D
 
 func _ready():
 	connect_area_2d.body_entered.connect(_on_body_entered)
+	connect_area_2d.body_exited.connect(_on_body_exited)
 	
 	
 	if (Global.is_white_turn):
-		modulate = p1_color
+		polygon_2d.self_modulate = p1_color
 		add_to_group("White")
 	else:
-		modulate = p2_color
+		polygon_2d.self_modulate = p2_color
 		add_to_group("Black")
 	
 	Global.is_white_turn = !Global.is_white_turn
